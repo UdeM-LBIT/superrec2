d759052946f160b43bc11e552b9d311ef470ee05
Mattéo Delabre <git.matteo@delab.re>
2022-02-10 17:59:30 -0500

    draw: Add back unit tests

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
+
 test:
 	python -m unittest discover --buffer --verbose
+
+.ONESHELL:
+SHELL=/usr/bin/bash
+fixtures:
+	for input in superrec2/render/fixtures/*/input.json; do
+	    for orient in horizontal vertical; do
+		./draw.py --orientation "$$orient" < "$$input" \
+	            > "$${input%input.json}output-$$orient.tex"
+	    done
+	done
 
 lint:
 	pylint --ignore-patterns "test_" *.py superrec2
---
+++ superrec2/render/test_draw.py
@@ -0,0 +1,42 @@
+import unittest
+import importlib.resources
+import subprocess
+from .model import Orientation
+
+
+class TestDraw(unittest.TestCase):
+    def test_fixtures(self):
+        with importlib.resources.path(__package__, "fixtures") as fixtures_path:
+            for fixture in fixtures_path.iterdir():
+                self.do_test_fixture(fixture)
+
+    def do_test_fixture(self, fixture):
+        input_path = fixture / "input.json"
+
+        for orientation in Orientation:
+            orientation_name = orientation.name.lower()
+            output_path = fixture / f"output-{orientation_name}.tex"
+
+            with open(output_path, "rb") as output_expect_file:
+                output_expect = output_expect_file.read()
+                result = subprocess.run(
+                    [
+                        "./draw.py",
+                        "--orientation",
+                        orientation_name,
+                        "--input",
+                        input_path
+                    ],
+                    capture_output=True,
+                    check=True,
+                )
+                self.assertEqual(
+                    result.stdout,
+                    output_expect,
+                    f"{fixture.name} ({orientation}): valid output"
+                )
+                self.assertEqual(
+                    result.stderr,
+                    b"",
+                    f"{fixture.name} ({orientation}): no error"
+                )
