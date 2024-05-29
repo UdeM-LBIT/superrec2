d42c92ee54dec2213472a3d180d5fb105110ffbf
MaelC001 <55336346+MaelC001@users.noreply.github.com>
2024-05-29 13:42:55 -0400

    with debug

--- src/superrec2/cli/reconcile.py
+++ src/superrec2/cli/reconcile.py
@@ -20,9 +20,11 @@ from ..compute.util import (
     history_generator,
     partial_history_projector,
     partial_history_generator,
+    HistoryBuilder,
 )
 from ..compute import superdtlx
-
+from sowing.node import Node
+from collections import Counter
 
 methods = {}
 
@@ -59,6 +61,212 @@ def all_solutions(run, costs, output):
 
     for solution in histories:
         yield solution.value
+
+@register_method
+def general_solution(run, a, output):
+    """Report a representative solution in each class."""
+
+    list_result = []
+    while True :
+        a,b,c = general_solution_copy(run, a, output)
+        print(len(list_result), c)
+        for i in list_result:
+            if i[0] != a:
+                print(i)
+                print("\n\n\n")
+                print(a,b)
+                print(len(list_result))
+                return
+                yield
+            if i[1] !=b:
+                print("numA",i[1],"-",b)
+            else:
+                print("numB",i[1],"-",b)
+        list_result.append([a,b])
+
+    return
+    yield
+
+def general_solution_copy(run, _, output):
+    """Report a representative solution in each class."""
+    def get_sub_history(hist) :
+        data, edge = get_data_edges(hist)
+        if data.apparent:
+            return HistoryBuilder(Node("continue")), hist
+        else:
+            h = HistoryBuilder(Node(data))
+            next = None
+            for i in edge:
+                t, n = get_sub_history(i)
+                h = h * t
+                if n != None:
+                    next = n
+            return h, next
+
+    def calc_freq(hist, frequence):
+        h, n = get_sub_history(hist)
+
+        if n == None :
+            return frequence
+
+        temp_n = n.node.data
+        if temp_n not in frequence:
+            frequence[temp_n] = {}
+        if h not in frequence[temp_n]:
+            frequence[temp_n][h] = 0
+        frequence[temp_n][h] += 1
+
+        for i in n.node.edges :
+            frequence = calc_freq(i, frequence)
+
+        return frequence
+
+    def add_2_history(genral, node, hist):
+        data, edge = get_data_edges(genral)
+        if data == 'continue':
+            newNode = HistoryBuilder(Node(node))
+            for i in hist:
+                newNode = newNode * i
+            return newNode
+        else:
+            newNode = HistoryBuilder(Node(data))
+            for i in edge:
+                newNode = newNode * add_2_history(i, node, hist)
+            return newNode
+
+
+    def construct_representative(genral, node, edge, frequence):
+        hist = []
+        for i in edge:
+            n, h = get_most_freq(i, frequence)
+            _, edge1 = get_data_edges(i)
+            hist.append(construct_representative(h, n, edge1, frequence))
+        genral = add_2_history(genral, node, hist)
+
+        return genral
+
+    def get_most_freq(classe, frequence):
+        data, edge = get_data_edges(classe)
+
+        max = [-1, None]
+        for i in frequence[data]:
+            if frequence[data][i] > max[0]:
+                max = [frequence[data][i], i]
+
+        return data, max[1]
+
+    def before_construct_representative(hist, frequence):
+        n, h = get_most_freq(hist, frequence)
+        edge = hist.node.edges if 'node' in dir(hist) else hist.value.edges
+        return construct_representative(h, n, edge, frequence)
+
+    def get_data_edges(hist):
+        d = dir(hist) 
+        if 'node' in d :
+            return hist.node.data, hist.node.edges
+        elif 'value' in d :
+            return hist.value.data, hist.value.edges
+        else :
+            return hist.data, hist.edges
+
+
+    import time
+
+    t0 = time.time()
+    result_number = run(structure=partial_history_generator @ (event_vector_pareto @ history_generator))
+    t1 = time.time()
+    #print(f"time run : {t1-t0}")
+    nbClass = []
+    nbClassOrigin = []
+    result_number_new = {}
+
+    for clas in result_number:
+        t = superdtlx.propagate_contents(History(run.keywords['setting'].host_tree, clas.value).prune_unsampled())
+        if t not in result_number_new :
+            result_number_new[t] = {}
+        for vector in result_number[clas]:
+            if vector not in result_number_new[t]:
+                result_number_new[t][vector] = set()
+            result_number_new[t][vector] = result_number_new[t][vector] | result_number[clas][vector]
+
+
+    print("aa",len(result_number), len(result_number_new))
+
+    for clas in result_number_new:
+        t = time.time()
+        #print(f"Classe :\n{h.value}")
+        frequence = {}
+        list_pareto = set()
+
+        s = 0
+        for vector in result_number_new[clas]:
+            s += len(result_number_new[clas][vector])
+            for hist in result_number_new[clas][vector]:
+                #breakpoint()
+                #list_pareto.add(History(run.keywords['setting'].host_tree, hist.value))
+                list_pareto.add(superdtlx.propagate_contents(History(run.keywords['setting'].host_tree, hist.value).prune_unsampled()))
+
+        nbClassOrigin.append(s)
+        nbClass.append(len(list_pareto))
+
+        #for h3 in list_pareto:
+            # print(f"Contient :\n{h3.event_tree}")
+        #    frequence = calc_freq(h3.event_tree, frequence)
+
+        #general = before_construct_representative(h, frequence)
+
+        #print(f"La fréquence est {frequence}")
+        #print(f"La classe a {nbClass[-1]} histoires")
+        #print(f"L'histoire générale :\n{general.value}")
+        #print(f"time calcul : {time.time()-t}\n\n")
+
+    #print(f"""On a un total de {len(nbClass)} classes, pour {sum(nbClass)}, {sorted(nbClass)} histoires et pour {sum(nbClassOrigin)} histoires à l'origine :
+#En moyenne {round(sum(nbClass)/len(nbClass),1)} histoires.
+#Les extrêmes : {min(nbClass)} et {max(nbClass)}.
+#Plus précisément : {"".join(f"\n- on a {y} classes qui ont {i} histoires" for i,y in dict(Counter(nb for nb in nbClass)).items())}""")
+
+    #t2 = time.time()
+    #print(f"time run all general : {t2-t1}")
+    #print(f"time global : {t2-t0}")
+
+    #import hashlib
+    #print(hashlib.sha256(str(result_number).encode()).hexdigest())
+
+    return result_number, sum(nbClass), sum(nbClassOrigin)
+    #yield
+
+@register_method
+def general_event_solution(run, _, output):
+    """Report a representative solution in each class."""
+    import time
+
+    t0 = time.time()
+    result_number = run(structure=event_vector_pareto @ history_generator)
+    t1 = time.time()
+    print(f"time run : {t1-t0}")
+    nbClass = []
+    nbClassOrigin = []
+    for h in result_number:
+        t = time.time()
+        print(f"Classe :\n{h}")
+        list_pareto = set()
+
+        for h2 in result_number[h]:
+            list_pareto.add(superdtlx.propagate_contents(History(run.keywords['setting'].host_tree, h2.value).prune_unsampled()))
+        nbClassOrigin.append(len(result_number[h]))
+        nbClass.append(len(list_pareto))
+
+        print(f"La classe a {nbClass[-1]} histoires")
+        print(f"time calcul : {time.time()-t}\n\n")
+    print(f"""On a un total de {len(nbClass)} classes, pour {sum(nbClass)} histoires et pour {sum(nbClassOrigin)} histoires à l'origine :
+En moyenne {round(sum(nbClass)/len(nbClass),1)} histoires.
+Les extrêmes : {min(nbClass)} et {max(nbClass)}.
+Plus précisément : {"".join(f"\n- on a {y} classes qui ont {i} histoires" for i,y in dict(Counter(nb for nb in nbClass)).items())}""")
+    t2 = time.time()
+    print(f"time run all general : {t2-t1}")
+    print(f"time global : {t2-t0}")
+    return
+    yield
 
 
 @register_method
